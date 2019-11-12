76d0b00617a522155b2e9a4fe28db62baabc9c31
Rory Garcia <rory.garcia1@gmail.com>
2019-11-12 12:36:44 -0800

    set db url in config to process.env

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 9000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: 'https://blooming-garden-38714.herokuapp.com/',
+  DATABASE_URL: process.env.DATABASE_URL,
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://rorygarcia@localhost/writual-test'
 }
