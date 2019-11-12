d52ccd00a6445d161b82480f963ea4509b24386e
Rory Garcia <rory.garcia1@gmail.com>
2019-11-12 12:55:28 -0800

    fix syntax error

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 9000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL  'postgresql://rorygarcia@localhost/writual'
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://rorygarcia@localhost/writual'
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://rorygarcia@localhost/writual-test'
 }
