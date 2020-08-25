7a98629b139875b83d23a67300d480cb12cf945e
Rory Garcia <rory.garcia1@gmail.com>
2020-08-25 00:07:49 -0700

    add quotes to feedback sender address

--- src/user_feedback-router.js
+++ src/user_feedback-router.js
@@ -48,7 +48,7 @@ user_feedbackRouter
         });
 
         let info = transporter.sendMail({
-          from: notifications@writualapp.com,   //sender address
+          from: 'notifications@writualapp.com',   //sender address
           to: 'feedback@writualapp.com',   //list of receivers
           subject: `Feedback from ${serializedUserFeedback.name}`,   //Subject line
           text: serializedUserFeedback.title + ' ' + serializedUserFeedback.description + ' ' + serializedUserFeedback.email,   //plain text body
